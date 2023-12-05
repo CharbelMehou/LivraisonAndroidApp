45244e1978675433cfce03ae099d8d61c2e2ecb8
charbel-mehou <charbel.mehou@groupe-esigelec.org>
2023-12-05 10:59:29 +0100

    ✨Product repository creation

--- AndroidProject/app/src/main/java/com/example/livraison/repository/ProductRepository.java
+++ AndroidProject/app/src/main/java/com/example/livraison/repository/ProductRepository.java
@@ -1,4 +1,21 @@
 package com.example.livraison.repository;
 
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class ProductRepository {
+    private FirebaseFirestore db;
+
+    public ProductRepository() {
+        db = FirebaseFirestore.getInstance();
+    }
+
+    public void getProducts(EventListener<QuerySnapshot> listener) {
+        db.collection("produit")
+                .orderBy("nom", Query.Direction.ASCENDING)
+                .addSnapshotListener(listener);
+    }
+
 }
