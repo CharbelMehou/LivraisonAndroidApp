0716395c104fff98a45be81f040ed10ebc6762cc
charbel-mehou <charbel.mehou@groupe-esigelec.org>
2023-12-05 10:57:48 +0100

    ✨Creation of the displaying and order features

--- AndroidProject/app/src/main/java/com/example/livraison/view/adapter/ProductAdapter.java
+++ AndroidProject/app/src/main/java/com/example/livraison/view/adapter/ProductAdapter.java
@@ -1,9 +1,12 @@
 package com.example.livraison.view.adapter;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,36 +20,77 @@ import java.util.ArrayList;
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.MyViewHolder> {
     private Context context;
     private ArrayList<Product> productArrayList;
+    private OnQuantityChangedListener quantityChangedListener;
 
-    public ProductAdapter(Context context, ArrayList<Product> productArrayList) {
-        this.context = context;
-        this.productArrayList = productArrayList;
+    // Interface de callback pour notifier des changements de quantité
+    public interface OnQuantityChangedListener {
+        void onQuantityChanged(Product product, String newQuantity);
     }
 
+    // Constructeur de l'adaptateur
+    public ProductAdapter(Context context, ArrayList<Product> productArrayList, OnQuantityChangedListener listener) {
+        this.context = context;
+        this.productArrayList = productArrayList;
+        this.quantityChangedListener = listener;
+    }
+
+    // Méthodes de RecyclerView.Adapter...
     @NonNull
     @Override
-    public ProductAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(context).inflate(R.layout.item, parent, false);
         return new MyViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ProductAdapter.MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         Product product = productArrayList.get(position);
         holder.nomProduct.setText(product.getNom());
-        holder.quantityProduct.setText(product.getQuantity());
+        holder.quantityEditText.setText(product.getQuantity());
+        holder.quantityEditText.addTextChangedListener(new TextWatcherAdapter(product));
     }
+
     @Override
     public int getItemCount() {
         return productArrayList.size();
     }
+
+    // Classe ViewHolder...
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView nomProduct, quantityProduct;
+        TextView nomProduct;
+        EditText quantityEditText;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             nomProduct = itemView.findViewById(R.id.textViewNomProduct);
-            quantityProduct = itemView.findViewById(R.id.textViewQuantityProduct);
+            quantityEditText = itemView.findViewById(R.id.editTextQuantityProduct);
+        }
+    }
+
+    // Adapter pour TextWatcher avec référence au produit
+    private class TextWatcherAdapter implements TextWatcher {
+        private Product product;
+
+        public TextWatcherAdapter(Product product) {
+            this.product = product;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            // Pas de besoin d'implémentation ici
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            // Rien ici non plus
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            // Notifiez l'activité du changement de quantité ici
+            if (quantityChangedListener != null) {
+                quantityChangedListener.onQuantityChanged(product, s.toString());
+            }
         }
     }
 }
--- AndroidProject/app/src/main/java/com/example/livraison/viewmodel/ProductionSelectionViewModel.java
+++ AndroidProject/app/src/main/java/com/example/livraison/viewmodel/ProductionSelectionViewModel.java
@@ -8,75 +8,110 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.livraison.R;
+import com.example.livraison.model.Order;
 import com.example.livraison.model.Product;
 import com.example.livraison.view.adapter.ProductAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
-public class ProductionSelectionViewModel extends AppCompatActivity {
+public class ProductionSelectionViewModel extends AppCompatActivity implements ProductAdapter.OnQuantityChangedListener {
 
     private FirebaseFirestore db;
     private RecyclerView productsRecyclerView;
     private ArrayList<Product> productArrayList;
-    ProgressDialog progressDialog;
-    ProductAdapter productAdapter;
+    private ProgressDialog progressDialog;
+    private ProductAdapter productAdapter;
 
-    // Appelé lors de la création de l'activité
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_production_selection_view_model);
 
-
-        progressDialog=new ProgressDialog(this);
+        progressDialog = new ProgressDialog(this);
         progressDialog.setCancelable(false);
         progressDialog.setMessage("Fetching Data....");
         progressDialog.show();
 
-
-        productsRecyclerView=findViewById(R.id.products_recycler_view);
+        productsRecyclerView = findViewById(R.id.products_recycler_view);
         productsRecyclerView.setHasFixedSize(true);
         productsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        db=FirebaseFirestore.getInstance();
-        productArrayList=new ArrayList<Product>();
-        productAdapter=new ProductAdapter(ProductionSelectionViewModel.this,productArrayList);
-
+        db = FirebaseFirestore.getInstance();
+        productArrayList = new ArrayList<>();
+        productAdapter = new ProductAdapter(this, productArrayList, this);
         productsRecyclerView.setAdapter(productAdapter);
+
         EventChangeListener();
+
+        Button submitOrderButton = findViewById(R.id.submit_order_button);
+        submitOrderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<Product> selectedProducts = new ArrayList<>();
+                for (Product product : productArrayList) {
+                    if (product.getQuantity() != null && !product.getQuantity().isEmpty()) {
+                        selectedProducts.add(product);
+                    }
+                }
+
+                String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                String address = "User address here"; // N'oublie pas de remplacer par l'adresse réelle
+                String deliveryDate = "Delivery date here"; // Remplacer par la date réelle
+
+                Order newOrder = new Order(userId, selectedProducts, address, deliveryDate);
+
+                db.collection("orders").add(newOrder)
+                        .addOnSuccessListener(documentReference -> {
+                            Toast.makeText(ProductionSelectionViewModel.this, "Order placed successfully!", Toast.LENGTH_LONG).show();
+                        })
+                        .addOnFailureListener(e -> {
+                            Toast.makeText(ProductionSelectionViewModel.this, "Failed to place order.", Toast.LENGTH_LONG).show();
+                        });
+            }
+        });
     }
-    private void EventChangeListener(){
+
+    private void EventChangeListener() {
         db.collection("produit")
                 .orderBy("nom", Query.Direction.ASCENDING)
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                        if (error !=null){
-                            if(progressDialog.isShowing())
+                        if (error != null) {
+                            if (progressDialog.isShowing())
                                 progressDialog.dismiss();
-                            Log.e("Firestore error",error.getMessage()) ;
+                            Log.e("Firestore error", error.getMessage());
                             return;
                         }
-                        for(DocumentChange dc:value.getDocumentChanges()){
-                            if(dc.getType()==DocumentChange.Type.ADDED){
+
+                        for (DocumentChange dc : value.getDocumentChanges()) {
+                            if (dc.getType() == DocumentChange.Type.ADDED) {
                                 productArrayList.add(dc.getDocument().toObject(Product.class));
                             }
                             productAdapter.notifyDataSetChanged();
-                            if(progressDialog.isShowing())
-                                progressDialog.dismiss();
                         }
+
+                        if (progressDialog.isShowing())
+                            progressDialog.dismiss();
                     }
                 });
     }
 
+    @Override
+    public void onQuantityChanged(Product product, String newQuantity) {
+        // Mettre à jour la quantité du produit
+        product.setQuantity(newQuantity);
+    }
 }
-
-
