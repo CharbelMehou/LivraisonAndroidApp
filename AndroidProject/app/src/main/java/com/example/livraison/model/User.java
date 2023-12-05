0d07aef30cfe3093dd1b4f874656ff55785c87d2
charbel-mehou <charbel.mehou@groupe-esigelec.org>
2023-12-05 10:59:02 +0100

    ✨user class and repository

--- AndroidProject/app/src/main/java/com/example/livraison/model/User.java
+++ AndroidProject/app/src/main/java/com/example/livraison/model/User.java
@@ -1,4 +1,50 @@
 package com.example.livraison.model;
 
 public class User {
+    private String userId;
+    private String address;
+    private String phoneNumber;
+    private String role;
+
+    public User() {
+    }
+
+    public User(String userId, String address, String phoneNumber, String role) {
+        this.userId = userId;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
--- AndroidProject/app/src/main/java/com/example/livraison/repository/UserRepository.java
+++ AndroidProject/app/src/main/java/com/example/livraison/repository/UserRepository.java
@@ -1,4 +1,25 @@
 package com.example.livraison.repository;
 
+import com.example.livraison.model.User;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class UserRepository {
+    private FirebaseFirestore db;
+
+    public UserRepository() {
+        db = FirebaseFirestore.getInstance();
+    }
+
+    public void addUser(User user, OnFirestoreTaskComplete onComplete) {
+        db.collection("users").document(user.getUserId()).set(user)
+                .addOnSuccessListener(aVoid -> onComplete.onSuccess())
+                .addOnFailureListener(e -> onComplete.onFailure(e.getMessage()));
+    }
+
+    // Ajoutez d'autres méthodes pour récupérer ou mettre à jour des informations sur les utilisateurs
+
+    public interface OnFirestoreTaskComplete {
+        void onSuccess();
+        void onFailure(String errorMessage);
+    }
 }
