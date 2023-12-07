f392c3f701ca430371e459a0efa05208e0154ddc
charbel-mehou <charbel.mehou@groupe-esigelec.org>
2023-12-07 18:03:17 +0100

    Class Order Update:isValidateByPlaneur,state,driverSelected

--- AndroidProject/app/src/main/java/com/example/livraison/model/Order.java
+++ AndroidProject/app/src/main/java/com/example/livraison/model/Order.java
@@ -3,19 +3,62 @@ package com.example.livraison.model;
 import java.util.ArrayList;
 
 public class Order {
+    private transient String tempId;
     private String userEmail;
     private ArrayList<Product> orderedProducts;
     private String address;
     private String deliveryDate;
+    private boolean isValidateByPlaneur;
+    private String state;
+    private String driverSelected;
 
     public Order() {
     }
 
-    public Order(String userEmail, ArrayList<Product> orderedProducts, String address, String deliveryDate) {
+    public Order(String  tempId,String userEmail, ArrayList<Product> orderedProducts, String address, String deliveryDate, boolean isValidateByPlaneur,String state,String driverSelected) {
+        this.tempId= tempId;
         this.userEmail = userEmail;
         this.orderedProducts = orderedProducts;
         this.address = address;
         this.deliveryDate = deliveryDate;
+        this.state=state;
+        this.isValidateByPlaneur=isValidateByPlaneur;
+        this.driverSelected=driverSelected;
+    }
+
+    public Order(String userEmail, ArrayList<Product> orderedProducts, String address, String deliveryDate, boolean isValidateByPlaneur, String state, String driverSelected) {
+        this.userEmail = userEmail;
+        this.orderedProducts = orderedProducts;
+        this.address = address;
+        this.deliveryDate = deliveryDate;
+        this.state = state;
+        this.isValidateByPlaneur=isValidateByPlaneur;
+
+        this.driverSelected = driverSelected;
+    }
+
+    public boolean getIsValidateByPlaneur() {
+        return isValidateByPlaneur;
+    }
+
+    public void setValidateByPlaneur(boolean validateByPlaneur) {
+        isValidateByPlaneur = validateByPlaneur;
+    }
+
+    public String getTempId() {
+        return tempId;
+    }
+
+    public void setTempId(String tempId) {
+        this.tempId = tempId;
+    }
+
+    public String getDriverSelected() {
+        return driverSelected;
+    }
+
+    public void setDriverSelected(String driverSelected) {
+        this.driverSelected = driverSelected;
     }
 
     public String getUserEmail() {
@@ -48,5 +91,12 @@ public class Order {
 
     public void setDeliveryDate(String deliveryDate) {
         this.deliveryDate = deliveryDate;
+    }
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
     }
 }
