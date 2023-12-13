cacffc428c5dfd3f7170439a4de456b75b93099f
charbel-mehou <charbel.mehou@groupe-esigelec.org>
2023-12-13 01:04:44 +0100

    ✨update dependancies

--- AndroidProject/app/build.gradle.kts
+++ AndroidProject/app/build.gradle.kts
@@ -35,6 +35,8 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation ("org.osmdroid:osmdroid-android:6.1.14")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.0")
     implementation("com.google.firebase:firebase-firestore:24.9.1")
     implementation("com.google.firebase:firebase-database:20.3.0")
     testImplementation("junit:junit:4.13.2")
