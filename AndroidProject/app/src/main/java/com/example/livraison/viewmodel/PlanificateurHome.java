8d37040f2c429e5a5cb8eadff25e6819523af506
charbel-mehou <charbel.mehou@groupe-esigelec.org>
2023-12-20 23:46:04 +0100

    ✨updating plannifcateur home:adding img and button

--- AndroidProject/app/src/main/java/com/example/livraison/viewmodel/PlanificateurHome.java
+++ AndroidProject/app/src/main/java/com/example/livraison/viewmodel/PlanificateurHome.java
@@ -31,14 +31,11 @@ import android.widget.Toast;
 
 public class PlanificateurHome extends AppCompatActivity {
     FirebaseAuth auth;
-    Button logOutbutton;
+    Button logOutbutton,setupDriverButton,setupItineraireButton;
     Button buttonSetData;
     TextView textView;
     FirebaseUser user;
-    RecyclerView recyclerView;
-    PlanificateurAdapter planificateurAdapter;
-    ArrayList<String> driversEmails=new ArrayList<>();
-    ArrayList<Order> orderArrayList = new ArrayList<>();
+
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     private ProgressDialog progressDialog;
 
@@ -89,84 +86,24 @@ public class PlanificateurHome extends AppCompatActivity {
         progressDialog.setCancelable(false);
         progressDialog.setMessage("Fetching Data....");
         progressDialog.show();
-
-        recyclerView = findViewById(R.id.order_recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        planificateurAdapter = new PlanificateurAdapter(this, orderArrayList,driversEmails);
-        recyclerView.setAdapter(planificateurAdapter);
-
-        loadOrders();
-        loadDriverEmails();
+        setupDriverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(getApplicationContext(),SetupDriver.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        setupItineraireButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(getApplicationContext(),SetupItineraire.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 
-    private void loadOrders() {
-        db.collection("orders")
-                .whereEqualTo("isValidateByPlaneur", false)
-                .addSnapshotListener((value, error) -> {
-                    if (error != null) {
-                        if (progressDialog.isShowing())
-                            progressDialog.dismiss();
-                        Toast.makeText(this, "Error loading orders: " + error.getMessage(), Toast.LENGTH_LONG).show();
-                        return;
-                    }
 
-                    for (DocumentChange dc : value.getDocumentChanges()) {
-                        Order order = dc.getDocument().toObject(Order.class);
-                        order.setTempId(dc.getDocument().getId());
-                        switch (dc.getType()) {
-                            case ADDED:
-                                if (!order.getIsValidateByPlaneur() && !orderArrayList.contains(order)) {
-                                    orderArrayList.add(order);
-                                }
-                                break;
-                            case MODIFIED:
-                                for (int i = 0; i < orderArrayList.size(); i++) {
-                                    if (orderArrayList.get(i).getTempId().equals(order.getTempId())) {
-                                        if (order.getIsValidateByPlaneur()) {
-                                            orderArrayList.remove(i);
-                                        } else {
-                                            orderArrayList.set(i, order);
-                                        }
-                                        break;
-                                    }
-                                }
-                                break;
-                            case REMOVED:
-                                orderArrayList.removeIf(o -> o.getTempId().equals(order.getTempId()));
-                                break;
-                        }
-                    }
-
-                    planificateurAdapter.notifyDataSetChanged();
-                    if (progressDialog.isShowing())
-                        progressDialog.dismiss();
-                });
-    }
-
-
-
-    private void loadDriverEmails() {
-        db.collection("users")
-                .whereEqualTo("role", "chauffeur")
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        driversEmails.clear();
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            String email = document.getString("email");
-                            if (email != null) {
-                                driversEmails.add(email);
-                            }
-                        }
-                        planificateurAdapter.setDriverEmails(driversEmails);
-                    } else {
-                        Toast.makeText(this, "Error loading driver emails", Toast.LENGTH_SHORT).show();
-                    }
-                    if (progressDialog.isShowing())
-                        progressDialog.dismiss();
-                });
-    }
 }
